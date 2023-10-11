b1bc4e734b103bd8a768b64a7405419a84a5310c
mksystem98 <147119824+mksystem98@users.noreply.github.com>
2023-10-11 20:51:09 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int numA = sc.nextInt();
+        int numB = sc.nextInt();
+
+        System.out.println(numA + numB);
+        System.out.println(numA - numB);
+        System.out.println(numA * numB);
+        System.out.println(numA / numB);
+        System.out.println(numA % numB);
+    }
+}
