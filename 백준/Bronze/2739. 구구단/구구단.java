b01b00f2f126f4a13d0a7ac96351950b8572c5a7
mksystem98 <147119824+mksystem98@users.noreply.github.com>
2024-02-14 11:38:47 +0900

    [Bronze V] Title: 구구단, Time: 236 ms, Memory: 18532 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int dan = scanner.nextInt();
+
+        printGuGuDan(dan);
+
+    }
+
+    public static void printGuGuDan(int dan) {
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(dan + " * " + i + " = " + dan*i);
+        }
+    }
+
+}
