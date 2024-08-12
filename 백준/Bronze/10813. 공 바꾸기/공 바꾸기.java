d763d566d7191412ccb18deb3d4949955218a52a
mksystem98 <147119824+mksystem98@users.noreply.github.com>
2024-08-12 15:47:32 +0900

    [Bronze II] Title: 공 바꾸기, Time: 196 ms, Memory: 18508 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int totalBasket = scanner.nextInt();
+
+        int[] basket = new int[totalBasket];
+
+        for (int i = 0; i < basket.length; i++) {
+            basket[i] = i+1;
+        }
+
+        int changeCount = scanner.nextInt();
+
+        for (int i = 0; i < changeCount; i++) {
+            int index1 = scanner.nextInt();
+            int index2 = scanner.nextInt();
+
+            int tmp = 0;
+
+            tmp = basket[index2-1];
+            basket[index2-1] = basket[index1-1];
+            basket[index1-1] = tmp;
+        }
+
+        for (int i = 0; i < basket.length-1; i++) {
+            System.out.print(basket[i] + " ");
+        }
+        System.out.println(basket[basket.length -1 ]);
+    }
+}
